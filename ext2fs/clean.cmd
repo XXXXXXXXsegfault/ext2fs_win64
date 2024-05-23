f4e0c54bb6f72516678b17f65c0407f54becca1b
XXXXXXXXsegfault <88300808+XXXXXXXXsegfault@users.noreply.github.com>
2024-05-23 21:31:20 +0800

    Update

--- ext2fs/clean.cmd
+++
@@ -1,6 +0,0 @@
-del /S /Q tmp_ext2_cli ext2_cli.exe
-rmdir tmp_ext2_cli
-del /S /Q tmp_ext2_format ext2_format.exe
-rmdir tmp_ext2_format
-del /S /Q tmp_ext4_format ext4_format.exe
-rmdir tmp_ext4_format
